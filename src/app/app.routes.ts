b3948ad7737ae6a5e07b655da1a0f9f588067892
glrmrissi <guilhermerissi04112006@gmail.com>
2025-12-11 21:57:41 -0300

    feat: new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Auth } from './auth/auth';
+import { Dashboard } from './dashboard/dashboard';
+import { AuthGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        component: Auth
+    },
+    {
+        path: 'dashboard',
+        component: Dashboard,
+        canActivate: [AuthGuard]
+    },
+    {
+        path: '**',
+        redirectTo: 'auth',
+    }
+];
