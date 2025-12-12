e91bab9693f0459d8f118f87947adb6c96a87f46
glrmrissi <guilhermerissi04112006@gmail.com>
2025-12-11 21:55:13 -0300

    feat: dashboard component

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { Dashboard } from "./dashboard";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [Dashboard],
+})
+
+export class DashboardModule {}
---
+++ src/app/dashboard/dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: false,
+  templateUrl: './dashboard.html',
+  styleUrls: ['./dashboard.scss'],
+})
+export class Dashboard {
+
+}
