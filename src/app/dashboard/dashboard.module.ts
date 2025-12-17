7af2518e71466830ff06f91f1a6c63fd0109606c
glrmrissi <guilhermerissi04112006@gmail.com>
2025-12-16 23:01:32 -0300

    feat: sidebar draft, dashboard draft and new generic dialog error

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Dashboard } from "./dashboard";
 import { CommonModule } from "@angular/common";
+import { SideNav } from "../side-nav/side-nav";
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, SideNav],
     declarations: [Dashboard],
 })
 
